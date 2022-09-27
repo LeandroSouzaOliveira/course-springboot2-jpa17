8816ee0e758e3543adad959b03df9971adea0cba
Leandro Souza Oliveira <leandro.oliveirasouza1@gmail.com>
2022-09-27 17:22:59 -0300

    Project created

---
+++ src/main/java/com/educandoweb/coursebootjpa/CourseBootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.coursebootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseBootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseBootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/coursebootjpa/CourseBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.coursebootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
