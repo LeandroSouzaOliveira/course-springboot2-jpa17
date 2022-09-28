eb90703b41b2b47f911581dcde7455a963d6590c
Leandro Souza Oliveira <leandro.oliveirasouza1@gmail.com>
2022-09-28 18:35:25 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/coursebootjpa/config/TesteConfig.java
+++ src/main/java/com/educandoweb/coursebootjpa/config/TesteConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.educandoweb.coursebootjpa.entities.Category;
 import com.educandoweb.coursebootjpa.entities.Order;
 import com.educandoweb.coursebootjpa.entities.OrderItem;
+import com.educandoweb.coursebootjpa.entities.Payment;
 import com.educandoweb.coursebootjpa.entities.Product;
 import com.educandoweb.coursebootjpa.entities.User;
 import com.educandoweb.coursebootjpa.entities.enums.OrderStatus;
@@ -80,5 +81,9 @@ public class TesteConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-21T19:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 }
